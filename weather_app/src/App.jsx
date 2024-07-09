cc08c2c2849ce46d7397182b907f9ff45c2bb744
Ishika-45 <bansalishika58@gmail.com>
2024-07-09 22:52:26 +0530

    Update

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import search from './assets/icons/search.svg'
+import { useStateContext } from './Context'
 
 function App() {
 
   const [input, setInput] = useState('')
+  // const {weather} = useStateContext()
+  // console.log(weather)
 
   return (
 
@@ -14,7 +17,7 @@ function App() {
       <nav className='w-full p-3 flex justify-between items-center'>
         <h1 className='font-bold tracking-wide text-3xl'>Weather App</h1>
         <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
-          <img src = {search} alt = "search" />
+          <img src = {search} alt = "search" className='w-[1.5rem] h-[1.5rem]'/>
           <input onKeyUp={(e) => {
             if (e.key === 'Enter') {
               // submit the form
--- weather_app/src/Components/BackgroundLayout.jsx
+++ weather_app/src/Components/BackgroundLayout.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useStateContext } from '../Context'
 
 const BackgroundLayout = () => {
+  const { weather } = useStateContext()
+  const [image, setImage] = useState(Clear)
+
   return (
     <div>
-      
+      BackgroundLayout
     </div>
   )
 }
-
 export default BackgroundLayout
+ 
