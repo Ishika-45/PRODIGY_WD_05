9e569cd310e19e785cf0204551a64dd3cbf1ca7c
Ishika-45 <bansalishika58@gmail.com>
2024-07-10 23:58:50 +0530

    Almot complete

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import search from './assets/icons/search.svg'
 import { useStateContext } from './Context'
+import {BackgroundLayout, WeatherCard, MiniCard} from './Components'
+
 
 function App() {
 
   const [input, setInput] = useState('')
-  // const {weather} = useStateContext()
+  const {weather, thisLocation, values} = useStateContext()
   // console.log(weather)
 
   return (
-
-    
-      
     <div className='w-full h-screen text-white px-8'>
       <nav className='w-full p-3 flex justify-between items-center'>
         <h1 className='font-bold tracking-wide text-3xl'>Weather App</h1>
@@ -25,9 +24,33 @@ function App() {
           }} type = "text" className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)}/>
         </div>
       </nav>
-    </div>
-     
-      
+      <BackgroundLayout></BackgroundLayout>
+      <main className='w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center'>
+        <WeatherCard
+          place={thisLocation}
+          windspeed={weather.windspeed}
+          humidity={weather.humidity}
+          temperature={weather.temp}
+         heatIndex={weather.heatindex}
+         iconString={weather.conditions}
+         conditions={weather.conditions}
+        />
+
+        <div className='flex justify-center gap-8 flex-wrap w-[60%]'>
+          {
+            values?.slice(1,7).map(curr => {
+              <MiniCard
+                key={curr.datetime}
+                time={curr.datetime}
+                temp={curr.temp}
+                iconString={curr.conditions}
+              />
+            })  
+          }
+
+        </div>
+      </main>
+    </div>   
   )
 }
 
